b534715860029b9f6a1ed211fde940063bc56c25
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-16 15:17:40 +0100

    Abstract for animals

---
+++ src/main/java/zoo/Animal.java
@@ -0,0 +1,11 @@
+package zoo;
+
+import java.util.Date;
+
+public abstract class Animal {
+	private String name;
+	private String sexe;
+	private Integer neededSpace;
+	private String food;
+	private Date birthDay;
+}
