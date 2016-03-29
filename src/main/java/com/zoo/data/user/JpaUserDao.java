696e96745f693716b62337bf7d28e9daaa45858e
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-29 16:19:28 +0200

    IMPLEMENT: JPA / DAO for user

---
+++ src/main/java/com/zoo/data/user/JpaUserDao.java
@@ -0,0 +1,8 @@
+package com.zoo.data.user;
+
+import com.zoo.data.JpaDao;
+import com.zoo.model.User;
+
+public class JpaUserDao extends JpaDao<String, User> implements UserDao{
+
+}
---
+++ src/main/java/com/zoo/data/user/UserDao.java
@@ -0,0 +1,8 @@
+package com.zoo.data.user;
+
+import com.zoo.data.Dao;
+import com.zoo.model.User;
+
+public interface UserDao extends Dao<String, User>{
+
+}
