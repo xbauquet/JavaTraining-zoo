8fd9f4cbc74ae4275fa58008ae93929739242986
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-30 09:30:08 +0200

    CLEAN

--- src/main/java/com/zoo/util/LifeCycleBean.java
+++ src/main/java/com/zoo/util/LifeCycleBean.java
@@ -3,12 +3,10 @@ package com.zoo.util;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.zoo.model.User;
-import com.zoo.service.user.UserService;
 
 @Singleton
 @Startup
@@ -16,8 +14,7 @@ public class LifeCycleBean {
 
 	private static final String ADMIN_EMAIL = "admin@admin.com";
 	private static final String ADMIN = "admin";
-	private static final String ADMIN_AUTHORITY = "admin";
-	
+
 	@PersistenceContext
 	EntityManager em;
 
@@ -28,8 +25,7 @@ public class LifeCycleBean {
 		admin.setFirstName(ADMIN);
 		admin.setLastName(ADMIN);
 		admin.setPass(ADMIN);
-		admin.setAuthority(ADMIN_AUTHORITY);
-		
+		admin.setAuthority(ADMIN);
 		em.persist(admin);
 	}
 
