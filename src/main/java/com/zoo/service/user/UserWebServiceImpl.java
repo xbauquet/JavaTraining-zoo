d2ca46e35b927d128b719e1f5f89f61432a67f88
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-04-04 15:16:40 +0200

    UPDATE user web service - all working

--- src/main/java/com/zoo/service/user/UserWebServiceImpl.java
+++ src/main/java/com/zoo/service/user/UserWebServiceImpl.java
@@ -3,23 +3,39 @@ package com.zoo.service.user;
 import java.util.Collection;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import com.zoo.model.User;
 
-public class UserWebServiceImpl implements UserWebService {
+@Path("/user")
+public class UserWebServiceImpl {
 
 	private static final String SUCCESS = "success";
 	@Inject
 	private UserService userService;
 
-	@Override
-	public String addNewUser(String email, String firstName, String lastName, String pass) {
-		userService.createUser(email, firstName, lastName, pass);
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addNewUser(@QueryParam("email") String email, @QueryParam("firstName") String firstName,
+			@QueryParam("lastName") String lastName, @QueryParam("pass") String pass) {
+		userService.saveUser(userService.createUser(email, firstName, lastName, pass));
 		return SUCCESS;
 	}
 
-	@Override
-	public String connection(String email, String pass) {
+	@POST
+	@Path("/connection")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String connection(@QueryParam("email") String email, @QueryParam("pass") String pass) {
 		User user = userService.findUserById(email);
 		if (user != null && user.isMyPassword(pass)) {
 			userService.setCurrentUser(user);
@@ -29,34 +45,49 @@ public class UserWebServiceImpl implements UserWebService {
 		}
 	}
 
-	@Override
-	public User findUserById(String email) {
+	@GET
+	@Path("/id/{email}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User findUserById(@PathParam("email") String email) {
 		return userService.findUserById(email);
 	}
 
-	@Override
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<User> findAllUser() {
 		return userService.findAllUser();
 	}
 
-	@Override
+	@GET
+	@Path("/current")
+	@Produces(MediaType.APPLICATION_JSON)
 	public User getCurrentUser() {
 		return userService.getCurrentUser();
 	}
 
-	@Override
-	public String deleteUser(String email) {
+	@POST
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUser(@QueryParam("email") String email) {
 		userService.removeUser(userService.findUserById(email));
 		return SUCCESS;
 	}
 
-	@Override
-	public String addNewAdmin(String email, String firstName, String lastName, String pass) {
-		userService.createAdmin(email, firstName, lastName, pass);
+	@POST
+	@Path("/addAdmin")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addNewAdmin(@QueryParam("email") String email, @QueryParam("firstName") String firstName,
+			@QueryParam("lastName") String lastName, @QueryParam("pass") String pass) {
+		userService.saveUser(userService.createAdmin(email, firstName, lastName, pass));
 		return SUCCESS;
 	}
 
-	@Override
+	@GET
+	@Path("/logout")
+	@Produces(MediaType.TEXT_PLAIN)
 	public String logout() {
 		userService.disconnectCurrentUser();
 		return SUCCESS;
